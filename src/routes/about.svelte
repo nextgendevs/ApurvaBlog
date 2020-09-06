6ef54f303efbda40e26763867cfbf9fd2ed82d69
Himank Jain <himankjain@vivaldi.net>
2020-09-06 13:10:01 +0530

    Routes and Graphql

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,16 @@
 <svelte:head>
-	<title>About</title>
+        <title>about</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>about this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>
+minimalist web design really let's the content stand out and shine. 
+this is why a simple website design is the first choice of so many artists, photographers, 
+and even some writers. they want their creative content to be the center of attention, 
+rather than design elements created by someone else. 
+</p>
+
+<p>this minimal blog is built with <a href="https://svelte.dev/">svelte</a> and <a href="https://strapi.io/">strapi</a> 
+images by <a href="https://unsplash.com/@glencarrie">glen carrie</a> from unsplash 
+</p>
---
+++ src/routes/articles/[slug].svelte
@@ -0,0 +1,74 @@
+<script context="module">
+        import ApolloClient, { gql } from 'apollo-boost';  
+        import moment from 'moment';
+
+        const blogQuery = gql`
+        query Blogs($Slug: String!) {
+                blogs: blogs(where: { Slug: $Slug }) {
+                        id
+                        Title
+                        Description
+                        Published
+                        Body
+                        author {
+                                username
+                        }
+                        Slug
+                }
+                }
+        `;
+        export async function preload({params, query}) {
+                const client = new ApolloClient({ 
+                        uri: 'https://localhost:1337/graphql',
+                        fetch: this.fetch
+                         });
+                const results = await client.query({
+                        query: blogQuery,
+                        variables: {"Slug" : params.slug} 
+                })
+                return {post: results.data.blogs}
+        }
+</script>
+
+<script>
+        export let post;
+</script>
+
+<style>
+        .content :global(h2) {
+                font-size: 1.4em;
+                font-weight: 500;
+        }
+        .content :global(pre) {
+                background-color: #f9f9f9;
+                box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
+                padding: 0.5em;
+                border-radius: 2px;
+                overflow-x: auto;
+        }
+        .content :global(pre) :global(code) {
+                background-color: transparent;
+                padding: 0;
+        }
+        .content :global(ul) {
+                line-height: 1.5;
+        }
+        .content :global(li) {
+                margin: 0 0 0.5em 0;
+        }
+</style>
+
+<svelte:head>
+        <title>an amazing article</title>
+</svelte:head>
+
+{#each post as post}
+                <h2>{post.Title}</h2>
+                <h3>{moment().to(post.Published)} by {post.author.username}</h3>
+
+                <div class='content'>
+                {@html post.Body} </div>
+
+{/each}
+
+<p>⇺<a href="articles"> back to articles</a></p>
